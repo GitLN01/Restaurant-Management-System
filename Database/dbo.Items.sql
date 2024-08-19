7f0871154f3308b26a192f2759b83f1707bcf999
Lazar Nikitovic <lazonikitovic@gmail.com>
2024-08-19 22:46:12 +0200

    Remove unnecessary files

--- Database/dbo.Items.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Items]
-(
-	[item_id] INT IDENTITY (1, 1) NOT NULL,
-    [name] NVARCHAR(50) NOT NULL, 
-    [price] DECIMAL(18, 2) NOT NULL, 
-    [category] NVARCHAR(50) NOT NULL,
-	PRIMARY KEY CLUSTERED ([item_id] ASC)
-)
--- Database/dbo.Orders.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Orders] (
-    [order_id]     INT      IDENTITY (1, 1) NOT NULL,
-    [customer_id]  INT      NOT NULL,
-    [time]         DATETIME NOT NULL,
-    [table_number] INT      NOT NULL,
-    PRIMARY KEY CLUSTERED ([order_id] ASC, [customer_id] ASC),
-    CONSTRAINT [FK_customer_id1] FOREIGN KEY ([customer_id]) REFERENCES [dbo].[Customers] ([customer_id])
-);
-
