a52788cf2c1879c8593f4958edf4dab268bcc5e2
Lazar Nikitovic <lazonikitovic@gmail.com>
2024-08-20 20:28:16 +0200

    Add ASP.NET Core project

---
+++ src/PresentationLayerWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PresentationLayerWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
